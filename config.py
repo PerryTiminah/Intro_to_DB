84663c3f86364c104691987c99c6cce515d23b1a
Perry Timinah <timnahwekalao@gmail.com>
2025-09-15 15:37:05 +0300

    updates

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Replace these values with your actual MySQL server credentials
+db_config = {
+    'host': 'localhost',      # usually localhost
+    'user': 'root',           # your MySQL username
+    'password': 'yourpassword'  # your MySQL password
+}
